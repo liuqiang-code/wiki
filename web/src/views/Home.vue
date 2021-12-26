bbad787b10b1a54741d50ae7774506a327188021
大强 <lz673253@163.com>
2021-12-26 21:42:06 +0800

    集成 Ant Design Vue，添加按钮示例

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="home">
+    <div :style="{ background: 'rgb(190, 200, 200)', padding: '26px 16px 16px' }">
+      <a-button type="primary" ghost>Primary</a-button>
+      <a-button ghost>Default</a-button>
+      <a-button type="dashed" ghost>Dashed</a-button>
+      <a-button danger ghost>Danger</a-button>
+      <a-button type="link" ghost>Link</a-button>
+    </div>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
