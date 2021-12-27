6661e7b41ed78b7b8b201aa91fbe2686158d979e
大强 <lz673253@163.com>
2021-12-27 22:06:59 +0800

    添加the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,20 @@
   <a-layout>
     <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      bigstrong's wiki
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
   import TheHeader from '@/components/the-header.vue';
+  import TheFooter from '@/components/the-footer.vue';
 
   export default defineComponent({
     name: 'app',
     components: {
-      TheHeader
+      TheHeader,
+      TheFooter
     }
   });
 </script>
