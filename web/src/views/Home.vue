8b862af8d4f19d2c586b1787505ee48b6105c516
大强 <lz673253@163.com>
2022-01-01 10:52:34 +0800

    添加axios拦截器配置请求响应日志

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -88,7 +88,6 @@ for (let i = 0; i < 23; i++) {
 export default defineComponent({
   name: 'Home',
   setup() {
-    console.log('setup')
     const ebooks = ref();
     const ebooks1 = reactive({books: []})
     onMounted(() => {
@@ -97,7 +96,6 @@ export default defineComponent({
         const data = response.data;
         ebooks.value = data.data;
         ebooks1.books = data.data;
-        console.log(response);
       })
     });
 
