e3d70d99d148327fbeeb6a854bb035e16ba1dd7e
大强 <lz673253@163.com>
2022-01-01 12:39:02 +0800

    添加AOP统计接口耗时，注释过滤器和拦截器

---
+++ src/main/java/com/bigstrong/wiki/aspect/LogAspect.java
@@ -0,0 +1,82 @@
+package com.bigstrong.wiki.aspect;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author BigStrong
+ * @date 2022/01/01
+ * @description AOP 打印接口耗时
+ */
+@Slf4j
+@Aspect
+@Component
+public class LogAspect {
+    /** 定义一个切点 */
+    @Pointcut("execution(public * com.bigstrong.*.controller..*Controller.*(..))")
+    public void controllerPointcut() {}
+
+    @Before("controllerPointcut()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        // 开始打印请求日志
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+
+        // 打印请求信息
+        log.info("------------- 开始 -------------");
+        log.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+        log.info("类名方法: {}.{}", signature.getDeclaringTypeName(), name);
+        log.info("远程地址: {}", request.getRemoteAddr());
+
+        // 打印请求参数
+        Object[] args = joinPoint.getArgs();
+        // LOG.info("请求参数: {}", JSONObject.toJSONString(args));
+
+        Object[] arguments  = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest
+                    || args[i] instanceof ServletResponse
+                    || args[i] instanceof MultipartFile) {
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        // 排除字段，敏感字段或太长的字段不显示
+        String[] excludeProperties = {"password", "file"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        log.info("请求参数: {}", JSONObject.toJSONString(arguments, excludefilter));
+    }
+
+    @Around("controllerPointcut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        // 排除字段，敏感字段或太长的字段不显示
+        String[] excludeProperties = {"password", "file"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        log.info("返回结果: {}", JSONObject.toJSONString(result, excludefilter));
+        log.info("------------- 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+}
--- src/main/java/com/bigstrong/wiki/filter/LogFilter.java
+++ src/main/java/com/bigstrong/wiki/filter/LogFilter.java
@@ -1,40 +1,40 @@
-package com.bigstrong.wiki.filter;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-/**
- * @author BigStrong
- * @date 2022/01/01
- * @description 打印接口耗时过滤器
- */
-@Slf4j
-@Component
-public class LogFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        // 打印请求日志
-        HttpServletRequest request = (HttpServletRequest)servletRequest;
-        log.info("----------------------LogFilter开始----------------------");
-        log.info("请求地址：{} {}", request.getRequestURL(), request.getMethod());
-        log.info("远程地址：{}", request.getRemoteAddr());
-        long startTime = System.currentTimeMillis();
-        filterChain.doFilter(servletRequest, servletResponse);
-        log.info("----------------------LogFilter结束 耗时：{} ms ----------------------",
-                System.currentTimeMillis() - startTime);
-    }
-
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
-    }
-}
+//package com.bigstrong.wiki.filter;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//
+///**
+// * @author BigStrong
+// * @date 2022/01/01
+// * @description 打印接口耗时过滤器
+// */
+//@Slf4j
+//@Component
+//public class LogFilter implements Filter {
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        Filter.super.init(filterConfig);
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        // 打印请求日志
+//        HttpServletRequest request = (HttpServletRequest)servletRequest;
+//        log.info("----------------------LogFilter开始----------------------");
+//        log.info("请求地址：{} {}", request.getRequestURL(), request.getMethod());
+//        log.info("远程地址：{}", request.getRemoteAddr());
+//        long startTime = System.currentTimeMillis();
+//        filterChain.doFilter(servletRequest, servletResponse);
+//        log.info("----------------------LogFilter结束 耗时：{} ms ----------------------",
+//                System.currentTimeMillis() - startTime);
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        Filter.super.destroy();
+//    }
+//}
