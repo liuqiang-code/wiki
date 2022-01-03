b387ed670ca1e9a18b29ae32621095d1890c8a80
大强 <lz673253@163.com>
2022-01-03 21:14:25 +0800

    前后端分页联动

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -87,13 +87,19 @@ export default defineComponent({
      **/
     const handleQuery = (params: any) => {
       loading.value = true;
-      axios.get("/ebook/list", params).then((response) => {
+      axios.get("/ebook/list", {
+        params: {
+          page: params.page,
+          size: params.size
+        }
+      }).then((response) => {
         loading.value = false;
         const data = response.data;
-        ebooks.value = data.data;
+        ebooks.value = data.data.data;
 
         // 重置分页按钮
         pagination.value.current = params.page;
+        pagination.value.total = data.data.total;
       });
     };
 
@@ -109,7 +115,10 @@ export default defineComponent({
     };
 
     onMounted(() => {
-      handleQuery({});
+      handleQuery({
+        page: 1,
+        size: pagination.value.pageSize
+      });
     });
 
     return {
--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -72,43 +72,38 @@
 import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
 import axios from 'axios';
 
-const listData: any = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
+// const listData: any = [];
+// for (let i = 0; i < 23; i++) {
+//   listData.push({
+//     href: 'https://www.antdv.com/',
+//     title: `ant design vue part ${i}`,
+//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+//     description:
+//         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+//     content:
+//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//   });
+// }
 
 export default defineComponent({
   name: 'Home',
   setup() {
     const ebooks = ref();
-    const ebooks1 = reactive({books: []})
     onMounted(() => {
       console.log("onMounted");
-      axios.get('/ebook/list').then(response => {
+      axios.get('/ebook/list', {
+        params: {
+          page: 1,
+          size: 1000
+        }
+      }).then(response => {
         const data = response.data;
-        ebooks.value = data.data;
-        ebooks1.books = data.data;
+        ebooks.value = data.data.data;
       })
     });
 
     return {
       ebooks,
-      ebooks2: toRef(ebooks1, 'books'),
-      listData,
-      pagination: {
-        onChange: (page: any) => {
-          console.log(page);
-        },
-        pageSize: 3,
-      },
       actions: [
         { type: 'StarOutlined', text: '156' },
         { type: 'LikeOutlined', text: '156' },
