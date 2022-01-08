2f13a0e97bbc8d20e8a0371424ecf7cc0ae5642f
大强 <lz673253@163.com>
2022-01-08 18:39:35 +0800

    优化编辑电子书接口

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -81,6 +81,7 @@
 import { defineComponent, onMounted, ref, computed, UnwrapRef, reactive } from 'vue';
 import axios from 'axios';
 import { message } from 'ant-design-vue';
+import {Tool} from '@/util/tool';
 
 export default defineComponent({
   name: 'AdminEbook',
@@ -198,7 +199,7 @@ export default defineComponent({
      */
     const edit = (record: any) => {
       modalVisible.value = true;
-      ebook.value = record;
+      ebook.value = Tool.copy(record);
     }
 
     /**
